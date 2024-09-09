d5e581bffa1d28deb798def240d2b26ba7149397
ZaidiSahab <1mustafazaidi@gmail.com>
2024-09-10 00:36:03 +0500

    Adding files of

---
+++ app.js
@@ -0,0 +1,40 @@
+function showTime() {
+
+
+    var date = new Date()
+
+    // document.write(date)
+
+    var h = date.getHours()
+    var m = date.getMinutes()
+    var s = date.getSeconds()
+    var session = "AM"
+
+
+    if (h == 0) {
+        h = 12
+    }
+    if (h > 12) {
+        h = h - 12
+        session = "PM"
+    }
+
+    h = (h < 10 ? "0" + h : h)
+    m = (m < 10 ? "0" + m : m)
+    s = (s < 10 ? "0" + s : s)
+
+    var time = h + ":" + m + ":" + s
+
+
+    
+     document.getElementById("value").
+    innerText =time
+    document.getElementById("value").textContent = time;
+
+    // document.write(" \n minutes", m)
+
+setTimeout(showTime , 1000)
+
+}
+
+showTime()
